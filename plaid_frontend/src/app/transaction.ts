8768bc0b704f52081fa15a82e9eaee890b8f276b
Joseph Hajjar <jhhajjar@gmail.com>
2025-11-16 15:14:25 -0800

    remove unnecessary fields in ts Transaction

--- plaid_frontend/src/app/transaction.ts
+++ plaid_frontend/src/app/transaction.ts
@@ -1,10 +1,7 @@
 export interface Transaction {
     transactionId: string,
     date: number,
-    authorized_date: number,
-    name: string,
     merchantName: string,
-    plaid_categories: string[],
     category: string,
     amount: number,
     includeInCalc: boolean
